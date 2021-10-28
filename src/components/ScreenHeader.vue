a012b8c3bd8a6886b0b0d0a2f81c78c9d41ddf8a
Nephis <nephis.alexandre@gmail.com>
2021-10-28 11:50:04 +0200

    new: header is now a component, 2nd part of home

---
+++ src/components/ScreenHeader.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="home-p1-right-header">
+          <span class="dot red-dot"></span>
+          <span class="dot yellow-dot"></span>
+          <span class="dot green-dot"></span>
+        </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+    name:"ScreenHeader"
+})
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,21 +5,18 @@
         <h3 class="home-title roboto title is-3">Hamakathon, by Le Hamak</h3>
         <hr />
         <h6 class="home-text roboto subtitle is-6">
-          THis is a test Bacon ipsum dolor amet doner turkey picanha pork short ribs swine. Cow
-          drumstick flank t-bone, ham hock beef ribs tail leberkas ground round
-          short ribs kevin venison biltong beef burgdoggen. Beef ribs boudin
-          doner tongue. Bacon chicken shankle sirloin ball tip shoulder.
+          THis is a test Bacon ipsum dolor amet doner turkey picanha pork short
+          ribs swine. Cow drumstick flank t-bone, ham hock beef ribs tail
+          leberkas ground round short ribs kevin venison biltong beef
+          burgdoggen. Beef ribs boudin doner tongue. Bacon chicken shankle
+          sirloin ball tip shoulder.
         </h6>
         <button class="button is-rounded purple-back">
           Join the experience
         </button>
       </div>
-      <div class="home-p1-right">
-        <div class="home-p1-right-header">
-          <span class="dot red-dot"></span>
-          <span class="dot yellow-dot"></span>
-          <span class="dot green-dot"></span>
-        </div>
+      <div class="screen home-p1-right">
+        <ScreenHeader />
       </div>
     </div>
     <img
@@ -28,12 +25,19 @@
       :src="doubleDown"
     />
   </div>
-  <div id="home-p2" class="home-p2"></div>
+  <div id="home-p2" class="home-p2">
+    <div class="overlap-parent">
+      <div class="screen overlap-a"><ScreenHeader /></div>
+      <div class="screen-brighter overlap-b"><ScreenHeader /></div>
+    </div>
+  </div>
 </template>
 <script lang="ts">
+import ScreenHeader from "@/components/ScreenHeader.vue";
 import { defineComponent } from "vue";
 import { useStore } from "vuex";
 export default defineComponent({
+  components: { ScreenHeader },
   name: "Home",
   setup(): any {
     const store: any = useStore();
